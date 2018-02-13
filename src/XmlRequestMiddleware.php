29b52d32c82dbc958a567b79fb697c86c65ad9fa
Loïc Rameau <lrameau@bloom-multimedia.fr>
2018-02-13 11:02:47 +0100

    Empty request no longer trigger fatal error.

--- src/XmlRequestMiddleware.php
+++ src/XmlRequestMiddleware.php
@@ -4,19 +4,26 @@ namespace XmlMiddleware;
 
 use Closure;
 
+/**
+ * Class XmlRequestMiddleware
+ * @package XmlMiddleware
+ */
 class XmlRequestMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
+     * @param  \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $request->merge($request->xml());
-        
+        // We pass an array to merge if the xml parsing somewhat failed, no error will trigger
+        // If the xml request contains an empty body
+        $request->merge($request->xml() ?: []);
+
         return $next($request);
     }
 }
